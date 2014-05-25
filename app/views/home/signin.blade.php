63709950364fec483fc62b490080c6fd79e7511b
Jerrod Bunce <jbunce@trucoin.com>
2014-05-25 13:12:35 -0600

    Fix signin page

--- app/views/home/signin.blade.php
+++ app/views/home/signin.blade.php
@@ -14,10 +14,14 @@
 		       <input type="password" placeholder="Password" class="form-control no-border">
 		    </div>
 		  </div>
-		  <button type="submit" class="btn btn-lg btn-default btn-block">Sign in</button>
+		  
 		  <a href="/login/facebook" class="btn btn-lg btn-primary btn-block"><i class="fa fa-facebook-square"></i> Facebook</a>
 		
 		  <a href="/login/google" class="btn btn-lg btn-primary btn-block google-btn"><i class="fa fa-google-plus-square"></i> Google+</a>
+		  
+  		  <div class="text-center m-t m-b">OR</div>
+  		  
+  		  <button type="submit" class="btn btn-lg btn-default btn-block">Sign in</button>
 		
 		  <div class="text-center m-t m-b"><a href="#"><small>Forgot password?</small></a></div>
 		  <div class="line line-dashed"></div>
