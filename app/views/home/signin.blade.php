477accf69f273c251f2f37f787f8a9cd5fcf029a
Jerrod Bunce <jbunce@trucoin.com>
2014-05-25 13:14:19 -0600

    fixing signup form

--- app/views/home/signin.blade.php
+++ app/views/home/signin.blade.php
@@ -15,14 +15,14 @@
 		    </div>
 		  </div>
 		  
+		  <button type="submit" class="btn btn-lg btn-default btn-block">Sign in</button>
+		  
+		  <div class="text-center m-t m-b">OR</div>
+		  
 		  <a href="/login/facebook" class="btn btn-lg btn-primary btn-block"><i class="fa fa-facebook-square"></i> Facebook</a>
 		
 		  <a href="/login/google" class="btn btn-lg btn-primary btn-block google-btn"><i class="fa fa-google-plus-square"></i> Google+</a>
-		  
-  		  <div class="text-center m-t m-b">OR</div>
-  		  
-  		  <button type="submit" class="btn btn-lg btn-default btn-block">Sign in</button>
-		
+
 		  <div class="text-center m-t m-b"><a href="#"><small>Forgot password?</small></a></div>
 		  <div class="line line-dashed"></div>
 		  <p class="text-muted text-center"><small>Do not have an account?</small></p>
