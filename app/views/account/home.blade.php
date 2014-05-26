7170556138c60c9a45f7189095dcda03da72dc1f
Jerrod Bunce <jbunce@trucoin.com>
2014-05-26 11:55:17 -0600

    add stuff

--- app/views/account/home.blade.php
+++ app/views/account/home.blade.php
@@ -29,25 +29,48 @@
                   </section>
                   
                   <div class="row">
-                  	<div class="col-md-12">
+                  	<div class="col-md-8">
 	                  	<section class="panel panel-default">
-	                  		<header class="panel-heading font-bold"> Inline form </header> 
+	                  		<header class="panel-heading font-bold">Add a Blood Glucose Reading</header> 
 	                  		<div class="panel-body"> 
 	                  			<form class="form-inline" role="form"> 
 	                  				<div class="form-group"> 
-	                  					<label class="sr-only" for="exampleInputEmail2">Email address</label> 
-	                  					<input type="email" class="form-control" id="exampleInputEmail2" placeholder="Enter email"> 
+	                  					<label class="sr-only" for="exampleInputEmail2">Reading</label> 
+	                  					<input type="text" class="form-control" id="exampleInputEmail2" placeholder="Enter meter reading"> 
 	                  				</div> 
 	                  				<div class="form-group">
-	                  					<label class="sr-only" for="exampleInputPassword2">Password</label> 
-	                  					<input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
+	                  					<label class="sr-only" for="exampleInputPassword2">Notes</label> 
+	                  					<input type="text" class="form-control" id="exampleInputPassword2" placeholder="ex. Before Lunch">
 	                  				</div>
-	                  				<div class="checkbox m-l m-r-xs">
-	                  					<label class="i-checks">
-	                  					<input type="checkbox"><i></i> Remember me </label>
+	                  				<button type="submit" class="btn btn-default">Submit</button> 
+	                  			</form>
+	                  		</div> 
+	                  	</section>
+                  	</div>
+                  	
+                  	<div class="col-md-8">
+	                  	<section class="panel panel-default">
+	                  		<header class="panel-heading font-bold">Add a meal</header> 
+	                  		<div class="panel-body"> 
+	                  			<form class="form-inline" role="form"> 
+	                  				<div class="form-group"> 
+	                  					<label class="sr-only" for="exampleInputEmail2">Carbs (g)</label> 
+	                  					<input type="text" class="form-control" id="exampleInputEmail2" placeholder="Enter amount of carbs"> 
+	                  				</div> 
+	                  				<div class="form-group">
+	                  					<label class="sr-only" for="exampleInputPassword2">Notes</label> 
+	                  					<input type="text" class="form-control" id="exampleInputPassword2" placeholder="ex. Before Lunch">
 	                  				</div>
-	                  				<button type="submit" class="btn btn-default">Sign in</button> 
-	                  				<a href="#modal-form" class="btn btn-success" data-toggle="modal">Form in a modal</a> 
+	                  				
+	                  				<div class="form-group">
+	                  					<select name="account" class="form-control m-b">
+	                  						<option>Breakfast</option>
+	                  						<option>Lunch</option>
+	                  						<option>Dinner</option>
+	                  						<option>Snack</option> 
+		                  				</select>
+	                  				</div>
+	                  				<button type="submit" class="btn btn-default">Submit</button> 
 	                  			</form>
 	                  		</div> 
 	                  	</section>
