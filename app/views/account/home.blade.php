5e2a8ecf9dfc2736c2fee394357b5820353731c7
Jerrod Bunce <jbunce@trucoin.com>
2014-05-26 09:41:44 -0600

    fix js

--- app/views/account/home.blade.php
+++ app/views/account/home.blade.php
@@ -452,27 +452,27 @@
       </section>
     </section>
   </section>
-  <script src="js/jquery.min.js"></script>
+  <script src="/assets/js/jquery.min.js"></script>
   <!-- Bootstrap -->
-  <script src="js/bootstrap.js"></script>
+  <script src="/assets/js/bootstrap.js"></script>
   <!-- App -->
-  <script src="js/app.js"></script>  
-  <script src="js/slimscroll/jquery.slimscroll.min.js"></script>
-    <script src="js/charts/easypiechart/jquery.easy-pie-chart.js"></script>
-  <script src="js/charts/sparkline/jquery.sparkline.min.js"></script>
-  <script src="js/charts/flot/jquery.flot.min.js"></script>
-  <script src="js/charts/flot/jquery.flot.tooltip.min.js"></script>
-  <script src="js/charts/flot/jquery.flot.spline.js"></script>
-  <script src="js/charts/flot/jquery.flot.pie.min.js"></script>
-  <script src="js/charts/flot/jquery.flot.resize.js"></script>
-  <script src="js/charts/flot/jquery.flot.grow.js"></script>
-  <script src="js/charts/flot/demo.js"></script>
+  <script src="/assets/js/app.js"></script>  
+  <script src="/assets/js/slimscroll/jquery.slimscroll.min.js"></script>
+    <script src="/assets/js/charts/easypiechart/jquery.easy-pie-chart.js"></script>
+  <script src="/assets/js/charts/sparkline/jquery.sparkline.min.js"></script>
+  <script src="/assets/js/charts/flot/jquery.flot.min.js"></script>
+  <script src="/assets/js/charts/flot/jquery.flot.tooltip.min.js"></script>
+  <script src="/assets/js/charts/flot/jquery.flot.spline.js"></script>
+  <script src="/assets/js/charts/flot/jquery.flot.pie.min.js"></script>
+  <script src="/assets/js/charts/flot/jquery.flot.resize.js"></script>
+  <script src="/assets/js/charts/flot/jquery.flot.grow.js"></script>
+  <script src="/assets/js/charts/flot/demo.js"></script>
 
-  <script src="js/calendar/bootstrap_calendar.js"></script>
-  <script src="js/calendar/demo.js"></script>
+  <script src="/assets/js/calendar/bootstrap_calendar.js"></script>
+  <script src="/assets/js/calendar/demo.js"></script>
 
-  <script src="js/sortable/jquery.sortable.js"></script>
-  <script src="js/app.plugin.js"></script>
+  <script src="/assets/js/sortable/jquery.sortable.js"></script>
+  <script src="/assets/js/app.plugin.js"></script>
 </body>
 </html>
 @stop
