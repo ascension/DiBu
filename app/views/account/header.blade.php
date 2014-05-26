a844219c398e2446038f99cc449db7e2240316b2
Jerrod Bunce <jbunce@trucoin.com>
2014-05-25 22:21:59 -0600

    fix css

--- app/views/account/header.blade.php
+++ app/views/account/header.blade.php
@@ -6,13 +6,13 @@
   <title>Scale | Web Application</title>
   <meta name="description" content="app, web app, responsive, admin dashboard, admin, flat, flat ui, ui kit, off screen nav" />
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /> 
-  <link rel="stylesheet" href="assets/css/bootstrap.css" type="text/css" />
-  <link rel="stylesheet" href="assets/css/animate.css" type="text/css" />
-  <link rel="stylesheet" href="assets/css/font-awesome.min.css" type="text/css" />
-  <link rel="stylesheet" href="assets/css/icon.css" type="text/css" />
-  <link rel="stylesheet" href="assets/css/font.css" type="text/css" />
-  <link rel="stylesheet" href="assets/css/app.css" type="text/css" />  
-  <link rel="stylesheet" href="js/calendar/bootstrap_calendar.css" type="text/css" />
+  <link rel="stylesheet" href="/assets/css/bootstrap.css" type="text/css" />
+  <link rel="stylesheet" href="/assets/css/animate.css" type="text/css" />
+  <link rel="stylesheet" href="/assets/css/font-awesome.min.css" type="text/css" />
+  <link rel="stylesheet" href="/assets/css/icon.css" type="text/css" />
+  <link rel="stylesheet" href="/assets/css/font.css" type="text/css" />
+  <link rel="stylesheet" href="/assets/css/app.css" type="text/css" />  
+  <link rel="stylesheet" href="/assets/js/calendar/bootstrap_calendar.css" type="text/css" />
   <!--[if lt IE 9]>
     <script src="js/ie/html5shiv.js"></script>
     <script src="js/ie/respond.min.js"></script>
