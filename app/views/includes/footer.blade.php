7699b506eec69f6e859d493333211234675ae036
Jerrod Bunce <jbunce@trucoin.com>
2014-05-28 22:04:29 -0600

    add js

--- app/views/includes/footer.blade.php
+++ app/views/includes/footer.blade.php
@@ -39,6 +39,7 @@
   <!-- / footer -->  <script src="js/jquery.min.js"></script>
   <!-- Bootstrap -->
   <script src="js/bootstrap.js"></script>
+  <script src="js/bootstrap-dialog.min.js"></script>
   <!-- App -->
   <script src="js/app.js"></script>  
   <script src="js/slimscroll/jquery.slimscroll.min.js"></script>
