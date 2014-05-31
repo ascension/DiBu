dca293ac1e2baac39a2e4a2c6db2e2a94c049a8e
Jerrod Bunce <jbunce@trucoin.com>
2014-05-31 00:55:08 -0600

    fix

--- app/views/account/home.blade.php
+++ app/views/account/home.blade.php
@@ -90,6 +90,7 @@
 							</div>
 						</div>
                   </div>
+                  </div>
                   	
                   <div class="row">
                   	<div class="col-md-6">
